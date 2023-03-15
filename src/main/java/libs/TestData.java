998d0a874636c37c4361037287e0faf9236cb262
Nataliia Golenko <nataliepinchuk@gmail.com>
2023-03-15 20:19:43 +0200

    final user

--- src/main/java/libs/TestData.java
+++ src/main/java/libs/TestData.java
@@ -1,6 +1,6 @@
 package libs;
 
 public class TestData {
-    public final static String LOGIN = "123newuser456";
+    public final static String LOGIN = "123newuser4567";
     public final static String PASSWORD = "qwerty1234";
 }
